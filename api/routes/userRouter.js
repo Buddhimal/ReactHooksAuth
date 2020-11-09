51029ffbc354c6dac2dc88b53478cc0de96c5c8c
Buddhimal Gunasekara <bnawgunasekara@gmail.com>
2020-11-10 01:46:33 +0530

    Delete user and Check auth

--- api/routes/userRouter.js
+++ api/routes/userRouter.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const auth = require("../middleware/auth")
 
 
 router.post('/register', async (req, res) => {
@@ -39,15 +41,81 @@ router.post('/register', async (req, res) => {
 
 
     } catch (err) {
-        res.status(500).json({err});
+        res.status(500).json({error: err.message});
     }
 
+});
+
+router.post("/login", async (req, res) => {
+    try {
+
+        const {email, password} = req.body;
+
+        if (!email || !password)
+            return res.status(400).json({msg: "Please Fill all the fields!"})
+
+        const user = await User.findOne({email: email});
+
+        if (!user)
+            return res.status(400).json({msg: "No User Account found!"})
+
+        const isMatch = await bcrypt.compare(password, user.password);
+
+        if (!isMatch)
+            return res.status(400).json({msg: "Invalied Password"});
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET);
+
+        return res.json({
+            token: token,
+            user: {
+                id: user.id,
+                displayName: user.displayName,
+                email: user.email
+            }
+        })
+
+
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
 })
 
 
-router.get("/test", (req, res) => {
-    res.send("Hello");
+router.delete("/delete", auth, async (req, res) => {
+    try {
+
+        const deletedUser = await User.findByIdAndDelete(req.user);
+
+        res.json(deletedUser);
+
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
 })
 
+
+router.post("/tokenIsValid", async (req, res) => {
+    try {
+
+        const token = req.header("x-auth-token");
+        if (!token)
+            return res.status(401).json(false);
+
+        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        if (!verified)
+            return res.status(401).json(false);
+
+        const user = await User.findById(verified.id);
+        if (!user)
+            return res.status(401).json(false);
+
+        return res.json(true);
+
+
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+})
 
 module.exports = router;
